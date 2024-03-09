068329d08c57bd8e23cc63e87d9cb0865aa0811c
KesavaS2 <162824465+KesavaS2@users.noreply.github.com>
2024-03-09 14:35:49 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function decodeText() {
+  var inputText = document.getElementById('inputText').value;
+  var decodedText = inputText.replace(/enter/g, 'e')
+                              .replace(/imes/g, 'i')
+                              .replace(/ai/g, 'a')
+                              .replace(/ober/g, 'o')
+                              .replace(/ufat/g, 'u');
+  document.getElementById('decodedText').innerText = decodedText;
+  document.getElementById('icon').src = 'icon_decode.png';
+}
+
+function encodeText() {
+  var inputText = document.getElementById('inputText').value;
+  var encodedText = inputText.replace(/e/g, 'enter')
+                              .replace(/i/g, 'imes')
+                              .replace(/a/g, 'ai')
+                              .replace(/o/g, 'ober')
+                              .replace(/u/g, 'ufat');
+  document.getElementById('decodedText').innerText = encodedText;
+  document.getElementById('icon').src = 'icon_encode.png';
+}
